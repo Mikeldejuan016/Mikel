e877108342182e005f487fc8da87f32f6cffb091
mikel <mikel.dejuan@educa.madrid.org>
2024-10-31 09:50:30 +0100

    d

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title> Musica Trap </title>
+        <script>
+            alert("Hola fan de Kidd Keo")
+        </script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Kidd Keo, Trap, musica"/>
+    </head>
+    <body>
+        <h1> Indice </h1>
+        <img src="Imagenes/Kidd_Keo/Cara_KiddKeo.jpg" width="200" height="150">
+        <p style="font-family: Lucida Sans"> En esta pagina Web tratara sobre el artista de musica Trap de <strong> Kidd Keo </strong> </p>
+        <p style="font-family: Courier New"> Hablare sobre distintos temas como: </p>
+        <ol>
+            <li> <a href="Biografia.html"> Biografia </a> </li>
+            <li> <a href="Albunes.html"> Albumes </a> </li>
+            <ul>
+                <li> Canciones de cada album </li>
+                <li> Canciones mas escuchadas </li>
+            </ul>
+        </ol>
+        <h2> Podcast con Jordi Wild </h2>
+        <audio src="Audio/Podcast_JordiWild.mp3" controls></audio>
+    </body>
+</html>';
+?>
